daa32833ef2c8b22545b35451c796f5158d589b3
Wesleykrt7 <abassa.wesley@gmail.com>
2025-02-03 17:52:24 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'  # Clé secrète pour les sessions
 def est_authentifie():
     return session.get('authentifie')
 
-# Page d'accueil -> Redirige vers l'authentification
+# Page d'accueil -> Redirige automatiquement vers la page d'authentification
 @app.route('/')
 def accueil():
     return redirect(url_for('authentification'))
@@ -25,11 +25,32 @@ def authentification():
     return render_template('formulaire_authentification.html', error=False)
 
 # Route pour afficher les livres
-@app.route('/livres')
+@app.route('/livres', methods=['GET', 'POST'])
 def livres():
     if not est_authentifie():
         return redirect(url_for('authentification'))
 
+    # Ajouter un livre si le formulaire est soumis
+    if request.method == 'POST':
+        titre = request.form['titre']
+        auteur = request.form['auteur']
+        annee_publication = request.form['annee_publication']
+        genre = request.form['genre']
+        description = request.form['description']
+        disponible = request.form['disponible'] == 'on'
+
+        conn = sqlite3.connect('bibliotheque.db')
+        cursor = conn.cursor()
+        cursor.execute('''
+            INSERT INTO Livres (titre, auteur, annee_publication, genre, description, disponible)
+            VALUES (?, ?, ?, ?, ?, ?)
+        ''', (titre, auteur, annee_publication, genre, description, disponible))
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('livres'))  # Rafraîchir la page pour afficher le livre ajouté
+
+    # Affichage des livres
     conn = sqlite3.connect('bibliotheque.db')  
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM Livres')  
@@ -39,7 +60,7 @@ def livres():
     return render_template('livres.html', livres=livres)  
 
 # Route pour supprimer un livre
-@app.route('/supprimer_livre/<int:livre_id>', methods=['POST'])
+@app.route('/supprimer_livre/<int:livre_id>', methods=['GET'])
 def supprimer_livre(livre_id):
     if not est_authentifie():
         return redirect(url_for('authentification'))
